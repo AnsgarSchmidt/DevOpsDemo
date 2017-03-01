1fb6c50d6d4c5d94e988200b1ab969261ed8db84
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-03-01 14:54:56 +0100

    w

--- devops/loadtest.sh
+++ devops/loadtest.sh
@@ -1,3 +1,3 @@
-siege -v -c 100 -t 5M https://cebit2017-python.mybluemix.net/add/100
-siege -v -c   1 -t 5M https://cebit2017-python.mybluemix.net/add/100
-
+#!/usr/bin/env bash
+siege -v -c 100 -t 5M https://cebit2017-python.mybluemix.net/add/150
+siege -v -c   1 -t 1M https://cebit2017-python.mybluemix.net/add/100
