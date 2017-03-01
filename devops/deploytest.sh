e63a39a00040a8a53b3d3521efed729832358d62
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-03-01 14:40:44 +0100

    3

---
+++ devops/deploytest.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+while true
+ do
+  curl http://cebit2017-python.mybluemix.net/version
+ done
--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def Welcome():
 
 @app.route('/version')
 def version():
-    return "2"
+    return "1"
 
 @app.route('/add/<level>')
 def load(level):
